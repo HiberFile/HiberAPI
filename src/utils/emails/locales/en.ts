1fe4828c67a88027ddaaed97932ad62275536a1f
Bruno Paiva <ppaiva11144@gmail.com>
2021-11-18 09:07:13 +0100

    fix: completed translation

--- src/utils/emails/locales/en.ts
+++ src/utils/emails/locales/en.ts
@@ -10,17 +10,15 @@ export const checkEmail: CheckEmail = {
   subject: 'Please check your email address for HiberFile',
 }
 
-// TODO: translate
-
 export const resetPassword: ResetPassword = {
-  button: 'cliquez ici',
+  button: 'click here',
   get_help:
-    "Pour obtenir de l'aide, n'hésitez pas à nous contacter sur notre compte Twitter.",
-  salutations: 'Meilleures salutations,',
-  hf_team: "L'Équipe HiberFile,",
-  subject: 'Réinitialisez votre mot de passe HiberFile',
-  you_asked_for_reset: 'Vous avez perdu votre mot de passe et nous avez demandé de le réinitialiser.',
-  if_you_not_asked: 'Si vous n\'êtes pas à l\'origine de cette demande, ne cliquez pas!',
-  if_you_asked: 'Si vous êtes à l\'origine de cette demande,',
-  follow_instructions: 'et suivez les instructions.'
+    "For help, feel free to contact us on our Twitter account.",
+  salutations: 'Best regards,',
+  hf_team: "The HiberFile Team",
+  subject: 'Reset your HiberFile password',
+  you_asked_for_reset: 'You have lost your password and asked us to reset it.',
+  if_you_not_asked: 'If you are not the author of this request, do not click!',
+  if_you_asked: 'Otherwise,',
+  follow_instructions: 'and follow the instructions.'
 }
